d48366d9c410607f7f9a1fdbdf3b20f01b593aca
Rene Milk <rene.milk@wwu.de>
2016-12-06 14:49:58 +0100

    proper quoting

--- dune_utils.bash
+++ dune_utils.bash
@@ -1,22 +1,41 @@
 #!/bin/bash
 
+sd ()
+{
+  eval "$@"
+  ret=$?
+  if [ $ret = 0 ] ; then
+          notify-send -t 3000 "done" "$PWD"
+  else
+          notify-send -t 3000 --urgency=critical "FAILED" "$PWD"
+  fi
+  return $ret
+}
+
+function check_source() {
+  if [ -r "${1}" ] ; then
+    # shellcheck source=/dev/null
+    source "${1}" ;
+  fi
+}
+
 function getOptsFile( )
 {
-    if [ x${1} = x ] ; then
+    if [ "x${1}" = "x" ] ; then
         if [ -e config.opts.last ] ; then
             OPTS=$(readlink config.opts.last)
         else
             OPTS=config.opts/gcc
-            ln -sf ${OPTS} config.opts.last
+            ln -sf "${OPTS}" config.opts.last
         fi
     else
         OPTS=${1}
-        ln -sf ${OPTS} config.opts.last
+        ln -sf "${OPTS}" config.opts.last
     fi
 }
 
-source PATH.sh
+check_source PATH.sh
 if [[ $OPTS == *"icc"* ]]
 then
-  source /opt/intel.PATH
+  check_source /opt/intel.PATH
 fi
